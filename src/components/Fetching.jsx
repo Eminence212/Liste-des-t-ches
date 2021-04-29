9c70bfc0bb461719adce451a2bad889de395f06a
Eminence <mulemanowa@gmail.com>
2021-04-29 16:17:57 +0200

    Essentiels de React

---
+++ src/components/Fetching.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {FaSpinner} from 'react-icons/fa'
+const Fetching = () =>(
+    <div className = "fetching" >
+        <FaSpinner className = "spinner" />
+    </div>
+)
+export default Fetching
